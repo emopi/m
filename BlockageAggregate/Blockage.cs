7e456c22965da90874e96f5c49c54f638fdee143
emre <bilkent35@gmail.com>
2021-03-07 14:22:56 +0300

    Create Blockage.cs

---
+++ BlockageAggregate/Blockage.cs
@@ -0,0 +1,20 @@
+﻿using Arkas.Domain.Core;
+using Arkas.Domain.Core.Attributes;
+
+namespace Arma.Services.MasterData.Domain.AggregatesModel.Crm.BlockageAggregate
+{
+    [EntityDescription("MasterData", "Blockage")]
+    public class Blockage : AggregateRoot
+    {
+        public string Name { get; private set; }
+
+        public Blockage()
+        {
+        }
+
+        public Blockage(string name)
+        {
+            Name = name;
+        }
+    }
+}
